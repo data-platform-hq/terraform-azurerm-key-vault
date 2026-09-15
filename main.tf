a959bf02823e31f3a27524ea03614046851f9199
Volodymyr Havadza <volodymyr_havadza@epam.com>
2026-09-15 11:04:00 +0300

    fix:prevent null into module

--- main.tf
+++ main.tf
@@ -87,7 +87,7 @@ resource "azurerm_monitor_diagnostic_setting" "this" {
 }
 
 resource "azurerm_key_vault_access_policy" "assigned_identity" {
-  for_each = { for i in var.key_vault_policy_config : i.object_id => i if i.object_id != null }
+  for_each = { for i in var.key_vault_policy_config : try(i.object_id, null) => i if try(i.object_id, null) != null }
 
   key_vault_id       = azurerm_key_vault.this.id
   tenant_id          = data.azurerm_client_config.this.tenant_id
